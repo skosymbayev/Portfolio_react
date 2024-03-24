4dbaab4a7a85b399b480e79965bb4a88cab2a6e8
Syrym <s.kosymbayev@gmail.com>
2024-03-24 18:24:11 +0500

    add dark mode, add custom localstorage hook

---
+++ src/components/BtnDarkMode/BtnDarkMode.jsx
@@ -0,0 +1,51 @@
+import {useEffect, useRef} from 'react';
+import detectDarkMode from '@/utils/detectDarkMode';
+import {useLocalStorage} from '@/utils/useLocalStorage';
+
+import './style.css';
+
+import sun from './sun.svg';
+import moon from './moon.svg';
+
+const BtnDarkMode = () => {
+    const [darkMode, setDarkMode] = useLocalStorage('portfolioDarkMode', detectDarkMode());
+    const btnRef = useRef(null);
+
+    useEffect(() => {
+        if( darkMode === 'dark' )
+        {
+            document.body.classList.add('dark')
+            btnRef.current.classList.add('dark-mode-btn--active')
+        }
+        else
+        {
+            document.body.classList.remove('dark')
+            btnRef.current.classList.remove('dark-mode-btn--active')
+        }
+    }, [darkMode]);
+
+    // Если изменились системные настройки
+    useEffect(() => {
+        window
+            .matchMedia('(prefers-color-scheme: dark)')
+            .addEventListener('change', (event) => {
+                const newColorScheme = event.matches ? 'dark' : 'light';
+                setDarkMode(newColorScheme);
+            })
+    }, []);
+
+    const toggleMode = () => {
+        setDarkMode((currentValue) => {
+            return currentValue === 'light' ? 'dark' : 'light';
+        });
+    }
+
+    return (
+        <button ref={btnRef} className="dark-mode-btn" onClick={toggleMode}>
+            <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
+            <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
+        </button>
+    )
+}
+
+export default BtnDarkMode;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,8 @@
 import {NavLink} from 'react-router-dom';
 
-import './style.css';
+import BtnDarkMode from '@/components/BtnDarkMode/BtnDarkMode';
 
-import sun from './../../assets/img/icons/sun.svg';
-import moon from './../../assets/img/icons/moon.svg';
+import './style.css';
 
 const Navbar = () => {
     const activeLink = 'nav-list__link nav-list__link--active';
@@ -17,10 +16,7 @@ const Navbar = () => {
                         <strong>Freelancer</strong> portfolio
                     </NavLink>
 
-                    <button className="dark-mode-btn">
-                        <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-                        <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
-                    </button>
+                    <BtnDarkMode />
 
                     <ul className="nav-list">
                         <li className="nav-list__item">
